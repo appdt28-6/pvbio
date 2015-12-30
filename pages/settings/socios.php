6a22e6527c3097af1cd30ced48427be1cb81ef38
appdt28-6 <rabackuu@gmail.com>
2015-12-29 18:16:09 -0600

    tabla

--- pages/settings/socios.php
+++ pages/settings/socios.php
@@ -273,15 +273,15 @@ $e=$_SESSION['inicia'];
     <!-- Metis Menu Plugin JavaScript -->
     <script src="../../bower_components/metisMenu/dist/metisMenu.min.js"></script>
 
-    <!-- Morris Charts JavaScript -->
-    <script src="../../bower_components/raphael/raphael-min.js"></script>
-    <script src="../../bower_components/morrisjs/morris.min.js"></script>
-    <script src=../"../js/morris-data.js"></script>
+    <!-- DataTables JavaScript -->
+    <script src="../../bower_components/DataTables/media/js/jquery.dataTables.min.js"></script>
+    <script src="../../bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
 
     <!-- Custom Theme JavaScript -->
-    
-</script>
-<script>
+    <script src="../../dist/js/sb-admin-2.js"></script>
+
+    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+    <script>
     $(document).ready(function() {
         $('#dataTables-example').DataTable({
                 responsive: true
